ba3b5faa283d26ae92e546c51273d2580d7dd98a
Artem Shadrunov <artemshadrunov@gmail.com>
2018-12-09 15:01:44 +0300

    Upd: Created Logger

---
+++ code/intersect-line-and-plane/Infrastructure/src/main/java/ru/unn/agile/intersect/infrastructure/Logger.java
@@ -0,0 +1,66 @@
+package ru.unn.agile.intersect.infrastructure;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class Logger implements ILogger{
+    private static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+
+    private final BufferedWriter logWriter;
+    private final String filename;
+
+    private static String now() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW, Locale.ENGLISH);
+        return sdf.format(cal.getTime());
+    }
+
+    public Logger(final String filename) {
+        this.filename = filename;
+
+        BufferedWriter logWriter = null;
+        try {
+            logWriter = new BufferedWriter(new FileWriter(filename));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.logWriter = logWriter;
+    }
+
+    @Override
+    public void log(final String s) {
+        try {
+            logWriter.write(now() + ": " + s);
+            logWriter.newLine();
+            logWriter.flush();
+        } catch (Exception e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public List<String> getLog() {
+        BufferedReader reader;
+        ArrayList<String> log = new ArrayList<String>();
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            String s = reader.readLine();
+
+            while (s != null) {
+                log.add(s);
+                s = reader.readLine();
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+
+        return log;
+    }
+}
