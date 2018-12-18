c454b2cb31554c8fc16dd998bd82f78447f379f6
nickshevr <nickshevr@yandex-team.ru>
2018-12-19 00:03:36 +0300

    lab-2: FibonacciHeap. Добавил model, BDD фреймворд для тестов

---
+++ code/fibonacci-heap/Model/src/test/java/ru.unn.agile.fibonacciHeap.model/FibbonaciHeapTestUtils.java
@@ -0,0 +1,54 @@
+package ru.unn.agile.fibonacciHeap.model;
+
+import com.mscharhag.oleaster.runner.OleasterRunner;
+import org.junit.runner.RunWith;
+
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.*;
+import static org.junit.Assert.*;
+
+@RunWith(OleasterRunner.class)
+public class FibbonaciHeapTestUtils {
+    private FibonacciHeap heap;
+    private Node firstNode;
+    private Node secondNode;
+    private Node thirdNode;
+    private double delta = 0.00001;
+    {
+        //BEGIN BDD CODE
+        describe("Constructor", () -> {
+            beforeEach(() -> {
+                heap = new FibonacciHeap();
+            });
+
+            describe("By default. Create instance", () -> {
+                it("with length = 0", () -> {
+                    assertEquals(heap.getRootAmount(), 0, delta);
+                });
+
+                it("with null rootNode", () -> {
+                   assertNull(heap.getRootNode());
+                });
+            });
+        });
+
+        describe("getNodeListSize method", () -> {
+            beforeEach(() -> {
+                firstNode = new Node(1);
+                secondNode = new Node(2);
+                thirdNode = new Node(3);
+            });
+
+            it("Should return 1 for single node", () -> {
+                assertEquals(heap.getNodeListSize(firstNode), 1, delta);
+            });
+
+            it("Should count nodes in tree", () -> {
+                firstNode.setChild(secondNode);
+                secondNode.setRight(thirdNode);
+
+                assertEquals(heap.getNodeListSize(firstNode), 3, delta);
+            });
+        });
+    }
+    //END BDD CODE
+}
