62456342e661d35d7bfc430adb2ba2f202ef9e54
Osechkin Sergey <osechkin.s.v@gmail.com>
2018-12-18 09:29:36 +0300

    fix emptystatment

--- code/mortgage-calculator/View/src/main/java/ru/unn/agile/MortgageCalculator/view/MortgageCalculatorForm.java
+++ code/mortgage-calculator/View/src/main/java/ru/unn/agile/MortgageCalculator/view/MortgageCalculatorForm.java
@@ -75,7 +75,6 @@ public final class MortgageCalculatorForm {
         for (String record : viewModel.getLog()) {
             str.append(record + "\n");
         }
-        ;
         textLog.setText(str.toString());
     }
 
