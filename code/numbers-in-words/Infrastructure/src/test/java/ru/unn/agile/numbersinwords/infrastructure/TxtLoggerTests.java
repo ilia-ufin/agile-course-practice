f82c6b4631763e9ecdcad5cda23fd7b92aae8df2
KryukovaPolina <paulina-17@yandex.ru>
2018-12-18 21:05:17 +0300

    even more consts

--- code/numbers-in-words/Infrastructure/src/test/java/ru/unn/agile/numbersinwords/infrastructure/TxtLoggerTests.java
+++ code/numbers-in-words/Infrastructure/src/test/java/ru/unn/agile/numbersinwords/infrastructure/TxtLoggerTests.java
@@ -5,19 +5,21 @@ import java.io.IOException;
 
 public class TxtLoggerTests {
     private static final String LOG_NAME = "testlog.log";
+    private static final String TEST_STRING = "test";
 
     @Test
     public void catchesClosedWriterWrite() throws IOException {
         TxtLogger logger = new TxtLogger(LOG_NAME);
 
         logger.close();
-        logger.log("test");
+        logger.log(TEST_STRING);
     }
 
     @Test
     public void catchesClosedWriterClose() throws IOException {
         TxtLogger logger = new TxtLogger(LOG_NAME);
 
+        logger.log(TEST_STRING);
         logger.getFileWriter().close();
         logger.close();
     }
