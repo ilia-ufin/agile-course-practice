70ff7bcd5c42b8dc330b6901792d140e78a9b0ac
Tatyana Golovanova <tgolovanova@avaya.ru>
2018-12-19 17:30:44 +0300

    Stage ViewModel:     Test - canGetScalesProperty

--- code/length-converter/ViewModel/src/main/java/ru/unn/agile/lengthconverter/viewmodel/ViewModel.java
+++ code/length-converter/ViewModel/src/main/java/ru/unn/agile/lengthconverter/viewmodel/ViewModel.java
@@ -1,4 +1,18 @@
 package ru.unn.agile.lengthconverter.viewmodel;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import ru.unn.agile.lengthconverter.model.LengthUnit;
+
 public class ViewModel {
+
+    private final ObjectProperty<ObservableList<LengthUnit>> units =
+            new SimpleObjectProperty<>(
+                    FXCollections.observableArrayList(LengthUnit.values()));
+
+    public ObjectProperty<ObservableList<LengthUnit>> unitsProperty() {
+        return units;
+    }
 }
--- code/length-converter/ViewModel/src/test/java/ru/unn/agile/lengthconverter/viewmodel/ViewModelTest.java
+++ code/length-converter/ViewModel/src/test/java/ru/unn/agile/lengthconverter/viewmodel/ViewModelTest.java
@@ -1,7 +1,14 @@
 package ru.unn.agile.lengthconverter.viewmodel;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.collections.ObservableList;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import ru.unn.agile.lengthconverter.model.LengthUnit;
 
 public class ViewModelTest {
     private ViewModel viewModel;
@@ -15,4 +22,15 @@ public class ViewModelTest {
     public void tearDown() {
         viewModel = null;
     }
+
+    @Test
+    public void canGetScalesProperty() {
+        ObjectProperty<ObservableList<LengthUnit>> unitsProperty
+                = viewModel.unitsProperty();
+
+        assertEquals(unitsProperty.get().get(0), LengthUnit.METERS);
+    }
+
+
+
 }
