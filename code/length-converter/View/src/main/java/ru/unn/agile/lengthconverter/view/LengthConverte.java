d702b7858f99ab7f1e11d3a8c14432225442bb15
Tatyana Golovanova <tgolovanova@avaya.ru>
2018-12-19 21:56:48 +0300

    Stage binding:       Start

--- code/length-converter/View/src/main/java/ru/unn/agile/lengthconverter/view/LengthConverte.java
+++ code/length-converter/View/src/main/java/ru/unn/agile/lengthconverter/view/LengthConverte.java
@@ -1,5 +1,11 @@
 package ru.unn.agile.lengthconverter.view;
 
+import javafx.fxml.FXML;
+import ru.unn.agile.lengthconverter.viewmodel.ViewModel;
+
 public class LengthConverte {
+    @FXML
+    private ViewModel viewModel;
+
 
 }
