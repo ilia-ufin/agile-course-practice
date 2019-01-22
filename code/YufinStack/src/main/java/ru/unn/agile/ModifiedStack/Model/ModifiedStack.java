dba30c927e7fc2d2611b7d411f5801aee992e8f3
Unknown <ilyuka95@gmail.com>
2019-01-22 11:22:43 +0300

    Create modified stack

---
+++ code/YufinStack/src/main/java/ru/unn/agile/ModifiedStack/Model/ModifiedStack.java
@@ -0,0 +1,72 @@
+package ru.unn.agile.ModifiedStack.Model;
+
+import java.util.*; 
+
+public class ModifiedStack { 
+    private Stack<Integer> StackElems; 
+    private Integer MinElem; 
+  
+    public ModifiedStack() {
+		StackElems = new Stack<Integer>(); 
+		} 
+
+    Integer getMin() { 
+        if (StackElems.isEmpty()) 
+            System.out.println("Stack is empty"); 
+  
+        else
+            return MinElem; 
+    } 
+
+    void peek() { 
+        if (StackElems.isEmpty()) 
+        { 
+            System.out.println("Stack is empty "); 
+            return; 
+        } 
+  
+        Integer top = StackElems.peek();
+
+        if (top < MinElem) 
+             return MinElem; 
+        else
+             return top; 
+    } 
+  
+    Integer pop() { 
+        if (StackElems.isEmpty()) 
+        { 
+            System.out.println("Stack is empty"); 
+            return; 
+        } 
+  
+        Integer top = StackElems.pop(); 
+
+        if (top < MinElem) 
+        { 
+            MinElem = 2*MinElem - top;
+			return	MinElem;		
+        } 
+        else
+			return	t;
+    } 
+  
+    void push(Integer x) { 
+        if (StackElems.isEmpty()) 
+        { 
+            MinElem = x; 
+            StackElems.push(x); 
+            return; 
+        }
+  
+        if (x < MinElem) 
+        { 
+            StackElems.push(2*x - MinElem); 
+            MinElem = x; 
+        } 
+  
+        else
+            StackElems.push(x); 
+   
+    } 
+}; 
