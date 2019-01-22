38281d0b053afd88d0961544d3a46c8f4ab8bddd
Artem <timofeynn@mail.ru>
2019-01-22 17:51:46 +0300

    Edited tests

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/complexnumber/model/MashaevArtemTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/complexnumber/model/MashaevArtemTest.java
@@ -16,4 +16,39 @@ public class MetelevEvgeniiTest {
         // Assert
         assertEquals(new ComplexNumber(4, 4), z);
     }
+
+    @Test
+    public void canMultiplyComplexNumbers() {
+        // Arrange
+        ComplexNumber z1 = new ComplexNumber(4, 1);
+        ComplexNumber z2 = new ComplexNumber(1, 4);
+
+        // Act
+        ComplexNumber z = z1.multiply(z2);
+
+        // Assert
+        assertEquals(new ComplexNumber(0, 17), z);
+    }
+
+    @Test
+    public void areComplexNumbersEqual() {
+        // Arrange
+        ComplexNumber z1 = new ComplexNumber(1, 1);
+        ComplexNumber z2 = new ComplexNumber(1, 1);
+
+        // Act
+        // Assert
+        assertTrue(z1.equals(z2));
+    }
+
+    @Test
+    public void areComplexNumbersNotEqual() {
+        // Arrange
+        ComplexNumber z1 = new ComplexNumber(2, 1);
+        ComplexNumber z2 = new ComplexNumber(1, 1);
+
+        // Act
+        // Assert
+        assertFalse(z1.equals(z2));
+    }
 }
