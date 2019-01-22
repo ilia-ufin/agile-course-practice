458f70e84bdf394adf45ba992d554d7e95378da7
Mary <fznamznon23@gmail.com>
2019-01-22 23:39:59 +0300

    Connect View and ViewModel components

--- code/ryabov-avl-tree/View/src/main/java/ru/unn/agile/AVL/view/AVLTree.java
+++ code/ryabov-avl-tree/View/src/main/java/ru/unn/agile/AVL/view/AVLTree.java
@@ -1,10 +1,15 @@
 package ru.unn.agile.AVL.view;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import ru.unn.agile.AVL.viewmodel.*;
 
 public class AVLTree {
+    @FXML
+    private ViewModel viewModel;
+
     @FXML
     private Button performAction;
     @FXML
@@ -16,5 +21,18 @@ public class AVLTree {
 
     @FXML
     void initialize() {
+        // Two-way binding hasn't supported by FXML yet, so place it in code-behind
+        actionType.valueProperty().bindBidirectional(viewModel.operationProperty());
+
+        key.textProperty().bindBidirectional(viewModel.keyProperty());
+        value.textProperty().bindBidirectional(viewModel.valueProperty());
+
+
+        performAction.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent event) {
+                viewModel.doAction();
+            }
+        });
     }
 }
