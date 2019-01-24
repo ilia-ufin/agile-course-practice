8f963fdf10bda64010a0ce2ff876b7c438f21139
Vladislav Ryabov <vlad.ryv@gmail.com>
2019-01-25 00:24:19 +0300

    ViewModel

--- code/rb-tree/ViewModel/src/main/java/ru/unn/agile/RB/ViewModel/ViewModel.java
+++ code/rb-tree/ViewModel/src/main/java/ru/unn/agile/RB/ViewModel/ViewModel.java
@@ -1,4 +1,70 @@
 package ru.unn.agile.RB.ViewModel;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import ru.unn.agile.RB.Model.RBNode;
+import ru.unn.agile.RB.Model.RBTree;
+
 public class ViewModel {
+
+    enum Status {
+        WAITING_FOR_INPUT("Waiting for input..."),
+        SUCCESS("Operation completed successfully"),
+        BAD_KEY_FORMAT("Key should be an integer"),
+        NOT_FOUND("No such key was found in the tree");
+
+        private final String name;
+
+        Status(final String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return name;
+        }
+
+
+    }
+
+    public void actionInsert() {
+        try {
+            tree.insert(Integer.parseInt(key.get()), value.get());
+            status = Status.SUCCESS;
+        } catch (NumberFormatException e) {
+            status = Status.BAD_KEY_FORMAT;
+        }
+    }
+
+    public void actionFind() {
+        try {
+        RBNode<Integer, String> found = tree.find(Integer.parseInt(key.get()));
+
+        if (found != null) {
+            status = Status.SUCCESS;
+        } else {
+            status = Status.NOT_FOUND;
+        }
+
+        } catch (NumberFormatException e) {
+            status = Status.BAD_KEY_FORMAT;
+        }
+    }
+
+    public StringProperty keyProperty() {
+        return key;
+    }
+
+    public StringProperty valueProperty() {
+        return value;
+    }
+
+    public Status status() {
+        return status;
+    }
+
+    private final StringProperty value = new SimpleStringProperty("");
+    private final StringProperty key   = new SimpleStringProperty("");
+    private Status status = Status.WAITING_FOR_INPUT;
+
+    private final RBTree<Integer, String> tree = new RBTree<>();
 }
--- code/rb-tree/ViewModel/src/test/java/ru/unn/agile/RB/ViewModel/ViewModelTests.java
+++ code/rb-tree/ViewModel/src/test/java/ru/unn/agile/RB/ViewModel/ViewModelTests.java
@@ -1,4 +1,82 @@
 package ru.unn.agile.RB.ViewModel;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ViewModelTests {
+
+    private ViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new ViewModel();
+    }
+
+    @After
+    public void tearDown() {
+        viewModel = null;
+    }
+
+    @Test
+    public void isKeyEmptyByDefault() {
+        assertEquals("", viewModel.keyProperty().get());
+    }
+
+    @Test
+    public void isValueEmptyByDefault() {
+        assertEquals("", viewModel.valueProperty().get());
+    }
+
+    @Test
+    public void isStatusEmptyByDefault() {
+        assertEquals(ViewModel.Status.WAITING_FOR_INPUT, viewModel.status());
+    }
+
+    @Test
+    public void insertingANode() {
+        viewModel.keyProperty().setValue("45678");
+        viewModel.valueProperty().setValue("asdadsasd");
+        viewModel.actionInsert();
+        assertEquals(ViewModel.Status.SUCCESS, viewModel.status());
+    }
+
+    @Test
+    public void insertingBadKeyFormat() {
+        viewModel.keyProperty().setValue("      123");
+        viewModel.valueProperty().setValue("");
+        viewModel.actionInsert();
+        assertEquals(ViewModel.Status.BAD_KEY_FORMAT, viewModel.status());
+    }
+
+    @Test
+    public void findANode() {
+        viewModel.keyProperty().setValue("123");
+        viewModel.valueProperty().setValue("");
+        viewModel.actionInsert();
+        viewModel.actionFind();
+        assertEquals(ViewModel.Status.SUCCESS, viewModel.status());
+    }
+
+    @Test
+    public void nodeNotFound() {
+        viewModel.keyProperty().setValue("123");
+        viewModel.valueProperty().setValue("");
+        viewModel.actionInsert();
+        viewModel.keyProperty().setValue("1");
+        viewModel.actionFind();
+        assertEquals(ViewModel.Status.NOT_FOUND, viewModel.status());
+    }
+
+    @Test
+    public void findingBadKeyFormat() {
+        viewModel.keyProperty().setValue("123");
+        viewModel.valueProperty().setValue("");
+        viewModel.actionInsert();
+        viewModel.keyProperty().setValue("    1");
+        viewModel.actionFind();
+        assertEquals(ViewModel.Status.BAD_KEY_FORMAT, viewModel.status());
+    }
 }
